700da130f92c902fa14d6673a7ca901ce2406d6e
jwing432 <jwing432@gmail.com>
2024-05-14 12:25:33 -0500

    Completed Tutorial video

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var game_manager = %"Game Manager"
+@onready var animation_player = $AnimationPlayer
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _on_body_entered(body):
+	game_manager.add_point() 
+	animation_player.play("pickup animation")
